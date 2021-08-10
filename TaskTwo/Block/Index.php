87cec8fb3dedea1b94723c1e30b71ed99a06b314
Qooqpll <alekseykarpov46@gmail.com>
2021-08-11 00:32:06 +0300

    task-2

---
+++ TaskTwo/Block/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Amasty\TaskTwo\Block;
+
+
+use Magento\Framework\View\Element\Template;
+
+class Index extends Template
+{
+    public function hello() {
+        return "hello world";
+    }
+
+}
